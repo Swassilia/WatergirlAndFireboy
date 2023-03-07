be74cdbac7af3085fa8bf9150e8481f99ada2990
SAID KARIM WASSILA p2100663 <wassila.said-karim@etu.univ-lyon1.fr>
2023-03-07 07:42:10 +0000

    oad New FileUpl

---
+++ src/core/Vect2.cpp
@@ -0,0 +1,19 @@
+
+
+// Vect2::Vect2(): x(0), y(0)
+// {
+// }
+// Vect2::Vect2(float nx, float ny): x(nx), y(ny)
+// {
+// }
+
+// Vect2::~Vect2():x(0), y(0)
+// {
+// }
+float distance( Vect2 b)
+{
+    return sqrt(a.x*b.x+a.y*b*y );
+}
+
+
+
