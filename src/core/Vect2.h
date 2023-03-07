7441c91a15dd1ce07f0c00652e5f0d5b048f48c8
SAID KARIM WASSILA p2100663 <wassila.said-karim@etu.univ-lyon1.fr>
2023-03-07 07:43:01 +0000

    Upload New File

---
+++ src/core/Vect2.h
@@ -0,0 +1,19 @@
+// class Vect2
+// {
+// private:
+//     float x, y;
+// public:
+//     Vect2(float nx, float ny);
+//     ~Vect2();
+// };
+struct Vect2
+{
+    float x, y;
+};
+float distance( Vect2 a ,Vect2 b);
+
+
+
+
+
+
