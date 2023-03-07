69564a2b0131fa5363b263e13230d0a202d5a731
SAID KARIM Wassila p2100663 <wassila.said-karim@univ-lyon1.fr>
2023-03-07 11:25:48 +0100

    changement Vect2 et Type.h

--- src/core/Vect2.cpp
+++ src/core/Vect2.cpp
@@ -1,19 +1,13 @@
+#include "Vect2.h"
 
-
-// Vect2::Vect2(): x(0), y(0)
-// {
-// }
-// Vect2::Vect2(float nx, float ny): x(nx), y(ny)
-// {
-// }
-
-// Vect2::~Vect2():x(0), y(0)
-// {
-// }
-float distance( Vect2 b)
+float distance( const Vect2 & a,const Vect2 & b)
 {
-    return sqrt(a.x*b.x+a.y*b*y );
+    return sqrt(a.x*b.x+ a.y*b*y);
 }
-
+Vect2 operator+(Vect2 a, Vect2 b)
+{
+    Vect2 res;
+    res.x=
+}
 
 
--- src/core/Vect2.h
+++ src/core/Vect2.h
@@ -8,12 +8,20 @@ struct Vect2
  * @param b 
 */
 float distance( Vect2 a ,Vect2 b);
+
 /**
  * @brief Calcule la distance entre deux vecteur2D
  * @param a 
  * @param b 
 */
-Vect2 operator+(Vect2 a, Vect b);
+Vect2 operator+(Vect2 a, Vect2 b);
+Vect2 operator-(Vect2 a, Vect2 b);
+Vect2 operator*(int lambda, Vect2 b);
+Vect2 operator*( Vect2 b, int lambda);
+Vect2 operator*(int lambda, Vect2 b);
+
+
+
 
 
 
