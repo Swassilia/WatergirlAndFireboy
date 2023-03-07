da4898fef473a2e2e47795668497da0c828ad94a
SAID KARIM WASSILA p2100663 <wassila.said-karim@etu.univ-lyon1.fr>
2023-03-07 07:43:44 +0000

    Upload New File

---
+++ src/core/Personnage.cpp
@@ -0,0 +1,45 @@
+#include <cassert>
+#include <cstddef>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "Personnage.h"
+
+
+
+Personnage::Personnage(const Type & t)
+{
+    type_p= t;
+    vivant=true;
+    
+}
+
+Personnage::~Personnage()
+{
+    vivant=false;
+    
+}
+
+void Personnage:: deplacer (const Plateau & p, int n )
+{
+    
+}
+
+float Personnage:: getx()
+{
+    return pos.x; 
+}
+float Personnage:: gety()
+{
+    return pos.y;
+}
+
+void Personnage:: sauter()
+{
+
+}
+
+void Personnage entrernom()
+{
+
+}
