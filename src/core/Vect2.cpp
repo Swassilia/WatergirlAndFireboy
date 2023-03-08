2f471f7bc5f0329673ccf5a988ec2dbfdb9a183e
SAID KARIM WASSILA p2100663 <wassila.said-karim@etu.univ-lyon1.fr>
2023-03-08 21:27:27 +0000

    Update Vect2.cpp

--- src/core/Vect2.cpp
+++ src/core/Vect2.cpp
@@ -1,13 +1,33 @@
 #include "Vect2.h"
 
+Vect2 make_Vect2 (float x, float y )
+ {
+     Point v;
+     v.x = x;
+     v.y = y ;
+     return v;
+ }
+
 float distance( const Vect2 & a,const Vect2 & b)
 {
-    return sqrt(a.x*b.x+ a.y*b*y);
+    return (sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y)));
 }
 Vect2 operator+(Vect2 a, Vect2 b)
 {
-    Vect2 res;
-    res.x=
+    return(make_Vect2(a.x+b.x,a.y+b.y));
 }
 
+Vect2 operator-(const Vect2 a, const Vect2& b)
+{
+   return(make_Vect2(a.x-b.x,a.y-b.y));
+}
 
+Vect2 operator*(int lambda, const Vect2& a)
+{
+    return (make_Vect2(a.x*lambda, a.y*lambda));
+}
+
+Vect2 operator*( const Vect2 & a,int lambda)
+{
+    return (make_Vect2(lambda*a.x, lambda*a.y));
+}
