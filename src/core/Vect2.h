aa05086ccc22038eed3c4924d85a32436251fc34
SAID KARIM WASSILA p2100663 <wassila.said-karim@etu.univ-lyon1.fr>
2023-03-11 22:35:38 +0000

    Update Vect2.h

--- src/core/Vect2.h
+++ src/core/Vect2.h
@@ -6,39 +6,45 @@ struct Vect2
     float x, y;
 };
 
+/**
+ * @brief Operateur = entre deux vecteurs
+ * @param a 
+ * @param b 
+*/
+
 Vect2 make_Vect2 (float x, float y );
 /**
  * @brief Calcule la distance entre deux vecteur2D
  * @param a 
  * @param b 
 */
-float distance( Vect2 a ,Vect2 b);
+float distance( const Vect2 & a ,const Vect2& b);
 
 /**
- * @brief Calcule la distance entre deux vecteur2D
+ * @brief Operateur + entre deux vecteurs
  * @param a 
  * @param b 
 */
-Vect2 operator+(Vect2 a, Vect2 b);
+Vect2 operator+(const Vect2& a, const Vect2 &b);
 
 /**
- * @brief Calcule la difference entre deux Vecteur2D
+ * @brief Operateur - entre deux vecteurs
  * @param a 
  * @param b 
 */
-Vect2 operator-(Vect2 a, Vect2 b);
+Vect2 operator-(const Vect2& a, const Vect2& b);
 /**
  * @brief Calcule produit un vecteur et un entié
  * @param lambda 
  * @param a
 */
-Vect2 operator*( Vect2 a, float lambda);
+Vect2 operator*( const Vect2 &a, const float lambda);
 /**
  * @brief Calcule produit un vecteur et un entié
  * @param lambda 
  * @param a 
 */
-Vect2 operator*(int lambda, Vect2 a);
+Vect2 operator*(int lambda, const Vect2 & a); 
 
 
 
