30ec09f7a1617260834d412a8e7e835616e12e81
SAID KARIM WASSILA p2100663 <wassila.said-karim@etu.univ-lyon1.fr>
2023-03-12 18:37:07 +0000

    Add new file

---
+++ src/txt/JeumodeText.h
@@ -0,0 +1,18 @@
+#ifndef _JeuTxt
+#define _JeuTxt
+
+#include "../core/Jeu.h"
+class JeuTxt
+{
+private:
+    Jeu jeu;
+
+public:
+    JeuTxt();
+    ~JeuTxt();
+    void Affichetxt();
+
+
+};
+
+#endif
