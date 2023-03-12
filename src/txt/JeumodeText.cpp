a0bb5a42cb9ec56c2f285b2b82562b4ef6d6563c
SAID KARIM WASSILA p2100663 <wassila.said-karim@etu.univ-lyon1.fr>
2023-03-12 18:37:54 +0000

    Update JeumodeText.cpp

--- src/txt/JeumodeText.cpp
+++ src/txt/JeumodeText.cpp
@@ -1,1 +1,27 @@
+#include "JeumodeText.h"
+#include <iostream>
 
+
+using namespace std;
+
+JeuTxt :: JeuTxt()
+{
+
+}
+JeuTxt :: ~JeuTxt()
+{
+    
+}
+
+void AfficheTxt()
+{
+    cout << dimx << " " << dimy << endl;
+    for(unsigned int y=0; y<dimy; ++y) {
+        for(unsigned int x=0; x<dimx; ++x) {
+            Pixel& pix = getPix(x,y);
+            cout << +pix.getRouge() << " " << +pix.getVert() << " " << +pix.getBleu() << " ";
+        }
+        cout << endl;
+    }
+}
+}
