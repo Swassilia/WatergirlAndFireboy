8b78ca81170daa24d9723a9ab17b897c06a04672
COLMEK ESRA p2103289 <esra.colmek@etu.univ-lyon1.fr>
2023-04-13 12:09:16 +0200

    ajout du menu

---
+++ src/menu/Menu.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include "Menu.h"
+
+const int MENU_MAX = 4;
+
+void menuAff()
+{
+	printf("\nMenu\n");
+	printf("Jeu mode texte\n");
+	printf("Jeu mode SDL\n");
+	/*printf("3 : Choix1\n");
+	printf("4 : Choix1\n");*/
+	//printf("0 : Exit\n");
+	printf("Votre choix?\n");
+	fflush(stdout);
+}
+
+
+ChoixMenu menuQuestion()
+{
+	ChoixMenu cm;
+	int i;
+	char dum[32];
+	bool ok=false;
+	do
+	{
+		if (scanf("%d",&i)!=1) scanf("%s",dum);
+		cm=((ChoixMenu)(i));
+		if ((cm<0) || (cm>=MENU_MAX)) 
+			printf("Erreur choix menu\n");
+		else ok=true;
+		fflush(stdout);
+	} while(!ok);
+	printf("\n"); fflush(stdout);
+	return cm;
+}
