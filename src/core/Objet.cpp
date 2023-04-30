0471337ed3cd83bb6c8d9d040dc1d8c1fdcd4b76
COLMEK ESRA p2103289 <esra.colmek@etu.univ-lyon1.fr>
2023-04-30 17:38:31 +0200

    commentaire Objet

--- src/core/Objet.cpp
+++ src/core/Objet.cpp
@@ -4,7 +4,8 @@
 #include "Objet.h"
 
 using namespace std;
- 
+
+//Constructeur par défaut de l'objet qui initialise le type de Objet à "Defaut" et la position à 0
 Objet::Objet(){
 
     typeO = Defaut;
@@ -12,6 +13,7 @@ Objet::Objet(){
 
 }
 
+//Constructeur par copie de l'objet qui initialise les données membres avec les paramètres
 Objet::Objet(const Type& t, const unsigned int x, const unsigned int y){
 
     typeO = t;
@@ -20,27 +22,33 @@ Objet::Objet(const Type& t, const unsigned int x, const unsigned int y){
 
 }
 
+//Destructeur de l'Objet 
 Objet::~Objet(){
 
     typeO = Defaut;
     posO= make_Vect2(0,0);
 
 }
+
+//Mutateur de l'Objet pour redéfinit la position de l'objet avec les valeurs passées en paramètre
 void Objet::setPos(const unsigned int x, const unsigned int y){
     posO.x = x;
     posO.y = y;
 }
 
+//Accesseur de l'Objet pour récupérer la position de l'objet
 Vect2 Objet::getPos()const  {
 
     return posO;
 }
 
+//Accesseur de l'Objet pour récupérer le type de l'Objet
 Type Objet::getType()const  {
 
     return typeO;
 }
 
+//Mutateur de l'Objet qui redéfinit le type de l'objet
 void Objet:: setType(const Type & t )
 {
     typeO=t;
--- src/core/Objet.h
+++ src/core/Objet.h
@@ -1,31 +1,89 @@
+/**
+@brief Module gérant les Objets du jeu
+@file Objet.h
+@date 2023/03/14
+*/
 #ifndef _Objet
 #define _Objet
-//#include "Plateau.h"
 #include "Type.h"
 #include "Vect2.h"
 
+/**
+@brief Un Objet = identifié par un type, une position et une direction
+*/
 class Objet {
 
     public:
-    
 
+    /**
+     * @brief Constructeur par défaut de la classe Objet
+     * 
+     */
     Objet();
 
+    /**
+     * @brief Constructeur par copie de la classe Objet
+     * 
+     * @param t Type
+     * @param posx unsigned int
+     * @param posy unsigned int
+     */
     Objet(const Type& t, unsigned int posx, unsigned int posy);
 
+    /**
+     * @brief Destructeur de la classe Jeu
+     * 
+     */
     ~Objet();
 
+    /**
+     * @brief accesseur de la classe Objet pour résupérer la position de l'objet
+     * 
+     * @return un vecteur Vect2
+     */
     Vect2 getPos()const;
+
+    /**
+     * @brief Mutateur de la classe Objet qui redéfinit la position de l'objet
+     * 
+     * @param x unsigned int x
+     * @param y unsigned int y
+     */
     void setPos(const unsigned int x, const unsigned int y);
+
+    /**
+     * @brief accesseur de la classe Objet pour récupérer le type de l'objet
+     * 
+     * @return un type Type
+     */
     Type getType() const;
+
+    /**
+     * @brief mutateur de la classe Objet pour redéfinir le type de l'objet
+     * 
+     * @param t Type
+     * 
+     */
     void setType(const Type & t );
 
+    /**
+     * @brief direction de l'objet de type int
+     * 
+     */
     int dir;
 
     private:
+    
+    /**
+     * @brief type de l'objet de type Type
+     * 
+     */
+    Type typeO;
 
-    Type typeO; //le type de l'objet
-
+    /**
+     * @brief position de l'objet de type Vect2
+     * 
+     */
     Vect2 posO; //position de l'objet
 
    
