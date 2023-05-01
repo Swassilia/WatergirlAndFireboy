04850f2c39a78c4575cbc5013e35a1275918a102
COLMEK ESRA p2103289 <esra.colmek@etu.univ-lyon1.fr>
2023-05-01 14:27:45 +0000

    Update JeuSdl.cpp

--- src/SDL/JeuSdl.cpp
+++ src/SDL/JeuSdl.cpp
@@ -159,7 +159,7 @@ SDLSimple::SDLSimple(): window(nullptr),renderer(nullptr){
         im_fond.loadFromFile("data/fond.jpg",renderer);
         im_diamond_eau.loadFromFile("data/diamondEau.png",renderer);
         im_diamond_feu.loadFromFile("data/diamondFeu.png",renderer);
-        im_bloc.loadFromFile("data/bloc.jpg",renderer);
+        im_bloc.loadFromFile("data/bloc.png",renderer);
         im_perso_eau.loadFromFile("data/Eau.png",renderer);
         im_perso_feu.loadFromFile("data/Feu.png",renderer);
         im_porte_eau.loadFromFile("data/porteEau.png",renderer);
